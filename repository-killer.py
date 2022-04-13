b447d2a1ce83041c157c0259b74f22e76e733506
dxlswb <103540884+dxlswb@users.noreply.github.com>
2022-04-12 21:45:02 -0400

    Create repository-killer.py

---
+++ repository-killer.py
@@ -0,0 +1,126 @@
+import requests
+import json
+import urllib.request as request
+import ssl
+from urllib import request
+from binascii import hexlify
+from email.mime.multipart import MIMEMultipart
+from smtplib import SMTP
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from smtplib import SMTP
+from email.mime.text import MIMEText
+import socket
+import telebot
+import logging
+import asyncio
+import sys
+from time import sleep
+from http.server import BaseHTTPRequestHandler
+import time
+import os
+import platform
+from datetime import datetime
+import binascii
+import ipaddress
+import platform
+
+os.system("clear")
+
+RR = "\033[29;m"
+def a(s):
+        for c in s + '\n':
+                sys.stdout.write(c)
+                sys.stdout.flush()
+                time.sleep(1./170)
+00
+
+
+a(RR+"█▀█ █▀▀ █▀█ █▀█ █▀ █ ▀█▀ █▀█ █▀█ █▄█ ▄▄ █▄▀ █ █░░ █░░")
+a(RR+"█▀▄ ██▄ █▀▀ █▄█ ▄█ █ ░█░ █▄█ █▀▄ ░█░ ░░ █░█ █ █▄▄ █▄▄")
+
+
+a(RR+"╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱   ╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱")
+a(RR+" Creador: dxlswb")
+a(RR+" Tiktok: dxlswb")
+a(RR+" instagram: X Swagyy X")
+a(RR+" YouTube: dxlswb")
+print("")
+a(RR+"╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱   ╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱")
+print("")
+a(RR+"╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱   ╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱")
+import requests
+import json
+import urllib.request as request
+import ssl
+from urllib import request
+from binascii import hexlify
+from email.mime.multipart import MIMEMultipart
+from smtplib import SMTP
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from smtplib import SMTP
+from email.mime.text import MIMEText
+import socket
+import telebot
+import logging
+import asyncio
+import sys
+from time import sleep
+from http.server import BaseHTTPRequestHandler
+import time
+import os
+import platform
+from datetime import datetime
+import binascii
+import ipaddress
+import platform
+
+os.system("clear")
+
+RR = "\033[29;m"
+def a(s):
+        for c in s + '\n':
+                sys.stdout.write(c)
+                sys.stdout.flush()
+                time.sleep(1./170)
+00
+
+
+a(RR+"█▀█ █▀▀ █▀█ █▀█ █▀ █ ▀█▀ █▀█ █▀█ █▄█ ▄▄ █▄▀ █ █░░ █░░")
+a(RR+"█▀▄ ██▄ █▀▀ █▄█ ▄█ █ ░█░ █▄█ █▀▄ ░█░ ░░ █░█ █ █▄▄ █▄▄")
+
+
+a(RR+"╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱   ╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱")
+a(RR+" Creador: dxlswb")
+a(RR+" Tiktok: dxlswb")
+a(RR+" instagram: X Swagyy X")
+a(RR+" YouTube: dxlswb")
+////////////////////////////////
+choice = int(input("1.-KILL REPOSITORY GIT HUB\n2.>
+b = 1
+if choice == 1:
+  op = int(input("1.-atake consentrado\n2.-atake bruto\nSeleccione una opsion:"))
+  if op == 1:
+   os.system("clear")
+   a(RR+"  
+█▀█ █▀▀ █▀█ █▀█ █▀ █ ▀█▀ █▀█ █▀█ █▄█ ▄▄ █▄▀ █ █░░ █░░ █▀▀
+█▀▄ ██▄ █▀▀ █▄█ ▄█ █ ░█░ █▄█ █▀▄ ░█░ ░░ █░█ █ █▄▄ █▄▄ ██▄")                           
+   a(RR+"  █░▒█ █▄▄▄ █░░░ █▄▄▄█ █░▒█ ░▒█░░ ░░ █▄▄█ █▄▄▄█ ░▒█░░")
+   print("\033[;32m")
+   username = ""
+   username = input("usuario:")
+   while True:
+         print (("[*]"),"REPORT-BOT FOR TIKTOK USER @", (username), (b))
+         sleep(1)
+         b = b + 1
+  elif op == 2:
+   os.system("clear")
+   a(RR+"  █▀▀█ █▀▀▀ █▀▀█ █▀▀▀█ █▀▀█ ▀▀█▀▀ ░░ █▀▀█ █▀▀▀█ ▀▀█▀▀")
+   a(RR+"  █▄▄▀ █▀▀▀ █▄▄█ █░░▒█ █▄▄▀ ░▒█░░ ▀▀ █▀▀▄ █░░▒█ ░▒█░░")            
+   print("\033[;32m")
+   username = ""
+   username = input("link de repositorio")
+   while True:
+         print (("[*]"),"KILLING THE REPOSITORY", (username), (b))
+         b = b + 1
